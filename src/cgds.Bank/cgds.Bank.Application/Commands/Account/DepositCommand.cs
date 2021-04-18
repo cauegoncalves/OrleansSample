a891bf792e25ed2735c97efd82246dc3f3f75e8c
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-18 01:31:05 -0300

    Deposit command implementation.

---
+++ src/cgds.Bank/cgds.Bank.Application/Commands/Account/DepositCommand.cs
@@ -0,0 +1,37 @@
+﻿using cgds.Bank.GrainInterfaces.Account;
+using MediatR;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cgds.Bank.Application.Commands.Account
+{
+    public class DepositCommand : IRequest
+    {
+        public int AgencyNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public class DepositCommandHandler : IRequestHandler<DepositCommand>
+    {
+
+        private readonly IGrainFactory _grainFactory;
+
+        public DepositCommandHandler(IGrainFactory grainFactory)
+        {
+            _grainFactory = grainFactory;
+        }
+        
+        public async Task<Unit> Handle(DepositCommand request, CancellationToken cancellationToken)
+        {
+            var accountGrain = _grainFactory.GetGrain<IAccountGrain>(request.AgencyNumber, request.AccountNumber);
+            await accountGrain.Deposit(request.Amount);
+            return Unit.Value;
+        }
+    }
+
+}
