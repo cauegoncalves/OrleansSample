bdf1f9a3e68b3c749a9665526cbf95fe98d30ca0
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-18 00:30:43 -0300

    Created account operation model

---
+++ src/cgds.Bank/cgds.Bank.Domain/Models/AccountOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cgds.Bank.Domain.Models
+{
+    public class AccountOperation
+    {
+
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
