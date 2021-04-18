453b23f63681a7c16b16dc04e0abf5946ba61b8c
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-18 00:34:48 -0300

    Account grain interface and state

---
+++ src/cgds.Bank/cgds.Bank.GrainInterfaces/Account/AccountState.cs
@@ -0,0 +1,17 @@
+﻿using cgds.Bank.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cgds.Bank.GrainInterfaces.Account
+{
+
+    public class AccountState
+    {
+
+        public decimal Balance { get; set; }
+        public List<AccountOperation> History { get; set; }
+
+    }
+
+}
---
+++ src/cgds.Bank/cgds.Bank.GrainInterfaces/Account/IAccountGrain.cs
@@ -0,0 +1,19 @@
+﻿using cgds.Bank.Domain.Models;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cgds.Bank.GrainInterfaces.Account
+{
+    public interface IAccountGrain : IGrainWithIntegerCompoundKey
+    {
+
+        Task Withdraw(decimal value);
+        Task Deposit(decimal value);
+        Task<decimal> GetBalance();
+        Task<List<AccountOperation>> GetHistory(DateTime startDate, DateTime endDate);
+
+    }
+}
