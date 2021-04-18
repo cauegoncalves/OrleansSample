e4f1d2897ef1601ab4846c3b8393146b0a8de227
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-18 01:18:48 -0300

    Time service implementation

---
+++ src/cgds.Bank/cgds.Bank.Services/Time/ITime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cgds.Bank.Services.Time
+{
+    public interface ITime
+    {
+
+        public DateTime Now { get; }
+
+    }
+}
---
+++ src/cgds.Bank/cgds.Bank.Services/Time/UTCTimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cgds.Bank.Services.Time
+{
+    public class UTCTimeService : ITime
+    {
+        public DateTime Now => DateTime.UtcNow;
+    }
+
+}
