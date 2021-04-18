7d475a0c9925665ff64f7baa09d9851b12856951
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-18 01:40:02 -0300

    Account operation dto created

---
+++ src/cgds.Bank/cgds.Bank.Domain/Dto/AccountOperationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cgds.Bank.Domain.Dto
+{
+    public class AccountOperationDto
+    {
+
+        public int AgencyNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal OperationAmount { get; set; }
+
+    }
+}
