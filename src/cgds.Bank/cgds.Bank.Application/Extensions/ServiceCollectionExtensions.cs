320f289b6904cad1e8e3d802c1fc9459bd6d5bb1
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-19 18:49:27 -0300

    Initializing mediator

---
+++ src/cgds.Bank/cgds.Bank.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace cgds.Bank.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddBankApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(ServiceCollectionExtensions));
+            return services;
+        }
+
+    }
+}
