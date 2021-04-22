dc7f648c8f1f9ba998c671e6f4d1256549ecf5da
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-21 21:59:38 -0300

    Created new exceptions

---
+++ src/cgds.Bank/cgds.Bank.Domain/Exceptions/InsufficientBalanceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cgds.Bank.Domain.Exceptions
+{
+    public class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException() : base("Insufficient balance to perform this operation.")
+        {
+        }
+    }
+}
---
+++ src/cgds.Bank/cgds.Bank.Domain/Exceptions/InvalidValueException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cgds.Bank.Domain.Exceptions
+{
+    public class InvalidValueException : Exception
+    {
+        public InvalidValueException(string valueName) : base($"Invalid value for the field '{valueName}'}")
+        {
+        }
+    }
+}
