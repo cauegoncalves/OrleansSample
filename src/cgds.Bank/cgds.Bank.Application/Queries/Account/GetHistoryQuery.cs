b98a3ebf193c239a563f4dc01e71dc4e07b1f37a
Caue Goncalves dos Santos <caue.goncalves@gmail.com>
2021-04-21 21:34:13 -0300

    Get history query and handler created

---
+++ src/cgds.Bank/cgds.Bank.Application/Queries/Account/GetHistoryQuery.cs
@@ -0,0 +1,36 @@
+﻿using cgds.Bank.Domain.Models;
+using cgds.Bank.GrainInterfaces.Account;
+using MediatR;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cgds.Bank.Application.Queries.Account
+{
+    public class GetHistoryQuery : IRequest<List<OperationHistoryEntry>>
+    {
+        public int AccountNumber { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public class GetHistoryQueryHandler : IRequestHandler<GetHistoryQuery, List<OperationHistoryEntry>>
+    {
+
+        private readonly IGrainFactory _grainFactory;
+
+        public GetHistoryQueryHandler(IGrainFactory grainFactory)
+        {
+            _grainFactory = grainFactory;
+        }
+
+        public Task<List<OperationHistoryEntry>> Handle(GetHistoryQuery request, CancellationToken cancellationToken)
+        {
+            var accountGrain = _grainFactory.GetGrain<IAccountGrain>(request.AccountNumber);
+            return accountGrain.GetHistory(request.StartDate, request.EndDate);
+        }
+    }
+}
